cf20898301da30504465ea9af0d9d744724994f3
Serhii Stotskyi <itp.stotsky@yandex.ua>
2018-06-17 19:55:22 +0300

    Added navigation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Auxed from '../../hoc/Auxed';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
-import classes from './Layout.css'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import classes from './Layout.css';
 
 const layout = (props) => (
     <Auxed>
         <Toolbar/>
+        <SideDrawer/>
         <main className={classes.Content}>
             {props.children}
         </main>
